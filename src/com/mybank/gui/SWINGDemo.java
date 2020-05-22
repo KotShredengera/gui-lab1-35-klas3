587938ddcbef1a89b125c2b09c605d945ecb5f77
Sir.Leviathan <sluvkann@gmail.com>
2020-05-22 13:30:24 +0300

    Add opportunity to work with customer's accounts

--- src/com/mybank/gui/SWINGDemo.java
+++ src/com/mybank/gui/SWINGDemo.java
@@ -14,8 +14,10 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 /**
  *
@@ -24,14 +26,25 @@ import javax.swing.JScrollPane;
 public class SWINGDemo {
     
     private final JEditorPane log;
-    private final JButton show;
     private final JComboBox clients;
+    
+    private final JButton show;
+    private final JButton deposit;
+    private final JButton withdraw;
+    
+    private final JTextField textField;
+    private final JComboBox accountTypes;
     
     public SWINGDemo() {
         log = new JEditorPane("text/html", "");
         log.setPreferredSize(new Dimension(450, 350));
         show = new JButton("Show report");
         clients = new JComboBox();
+        deposit = new JButton("Deposit");
+        withdraw = new JButton("Withdraw");
+        textField = new JTextField();
+        accountTypes = new JComboBox(new String[] { "Checking", "Savings" });
+        
         for (int i=0; i<Bank.getNumberOfCustomers();i++)
         {
             clients.addItem(Bank.getCustomer(i).getLastName()+", "+Bank.getCustomer(i).getFirstName());
@@ -42,12 +55,25 @@ public class SWINGDemo {
     private void launchFrame() {
         JFrame frame = new JFrame("MyBank clients");
         frame.setLayout(new BorderLayout());
-        JPanel cpane = new JPanel();
-        cpane.setLayout(new GridLayout(1, 2));
         
-        cpane.add(clients);
-        cpane.add(show);
-        frame.add(cpane, BorderLayout.NORTH);
+        JPanel cpaneTop = new JPanel();
+        JPanel cpaneBottom = new JPanel();
+        
+        cpaneTop.setLayout(new GridLayout(1, 2));
+        cpaneTop.add(clients);
+        cpaneTop.add(show);
+        
+        cpaneBottom.setLayout(new GridLayout(1, 2));
+        cpaneBottom.add(deposit);
+        cpaneBottom.add(withdraw);
+        cpaneBottom.add(accountTypes);
+        cpaneBottom.add(textField);
+        
+        frame.add(cpaneTop, BorderLayout.NORTH);
+        frame.add(cpaneBottom, BorderLayout.SOUTH);
+        
+        cpaneBottom.setVisible(false);
+        
         frame.add(new JScrollPane(log), BorderLayout.CENTER);
         
         clients.addActionListener(new ActionListener() {
@@ -55,6 +81,7 @@ public class SWINGDemo {
             public void actionPerformed(ActionEvent e) {
                 Customer current = Bank.getCustomer(clients.getSelectedIndex());                
                 String custInfo = getCustomerInfo(current);
+                cpaneBottom.setVisible(true);
                 log.setText(custInfo);
             }
         });
@@ -68,7 +95,22 @@ public class SWINGDemo {
                     report.append(getCustomerInfo(Bank.getCustomer(i)));
                 }
                 
+                cpaneBottom.setVisible(false);
                 log.setText(report.toString());
+            }
+        });
+        
+        deposit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleAccountOperation("deposit");
+            }
+        });
+        
+        withdraw.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleAccountOperation("withdraw");
             }
         });
         
@@ -99,6 +141,32 @@ public class SWINGDemo {
         }
         
         return info.toString();
+    }
+    
+    private void handleAccountOperation(String operationType) {
+        try {
+            Customer customer = Bank.getCustomer(clients.getSelectedIndex());
+
+            for (int i = 0; i < customer.getNumberOfAccounts(); i++) {
+                Account account = customer.getAccount(i);
+
+                if ((accountTypes.getSelectedItem() == "Checing" && account instanceof CheckingAccount) ||
+                    (accountTypes.getSelectedItem() == "Savings" && account instanceof SavingsAccount)) {
+                    if (operationType.equals("deposit")) {
+                        account.deposit(Double.parseDouble(textField.getText()));
+                    } else if (operationType.equals("withdraw")) {
+                        account.withdraw(Double.parseDouble(textField.getText()));
+                    }
+                    
+                    JOptionPane.showMessageDialog(log, "Operation succeeded.");
+                    return;
+                }
+            }
+
+            JOptionPane.showMessageDialog(log, "Customer doesn't have this type of account.");
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(log, "This operation could not be completed due to invalid input.");
+        }
     }
     
     public static void main(String[] args) {
